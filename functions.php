38b1a459b5211f29096ea6789afbed02f6dc59a4
Scott Hanson <papascott@gmail.com>
2025-04-21 16:34:59 +0200

    Use add_filter to insert proper uri for the feed icon

--- functions.php
+++ functions.php
@@ -6,3 +6,16 @@ add_action( 'wp_enqueue_scripts', function() {
     // Enqueue child theme's stylesheet
     wp_enqueue_style( 'wordland-child-style', get_stylesheet_uri(), ['retrospect-parent-style'] );
 });
+
+add_filter( 'render_block_core/social-link', function( $block_content, $block ) {
+    if ( isset( $block['attrs']['service'] ) && $block['attrs']['service'] === 'feed' ) {
+        // Replace href="#" or whatever is hardcoded with the dynamic feed link
+        $feed_url = esc_url( get_feed_link() );
+        $block_content = preg_replace(
+            '/<a[^>]+href="[^"]*"([^>]*)>/',
+            '<a href="' . $feed_url . '"$1>',
+            $block_content
+        );
+    }
+    return $block_content;
+}, 10, 2 );
