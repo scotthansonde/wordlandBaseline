cbd2740ef26561e20dbe52523de94c49263ffd39
Scott Hanson <papascott@gmail.com>
2025-07-30 08:42:11 +0200

    WIP Remove title and comments from linkblog-content

--- template-parts/linkblog-content.php
+++ template-parts/linkblog-content.php
@@ -5,7 +5,8 @@
  */
 ?>
 <div class="divStory">
-	<?php $has_title = get_the_title();
+	<?php
+
 
 	// Get the external URL from either wordland_linksTo or external_url custom field
 	$external_url = get_post_meta(get_the_ID(), 'wordland_linksTo', true);
@@ -16,16 +17,6 @@
 	// Fallback to post permalink if no external URL
 	$link_url = $external_url ? esc_url($external_url) : get_permalink();
 	?>
-	<div class="divStoryTitle">
-		<?php if ($has_title) : ?>
-			<?php the_title(); ?>
-		<?php endif; ?>
-	</div>
-	<?php if ($has_title) : ?>
-		<div class="XXXdivLineUnderStoryTitle">
-			<?php echo get_the_date(); ?> by <?php baseline_author_website_link(); ?>
-		</div>
-	<?php endif; ?>
 
 	<div class="divStoryBody">
 		<?php
@@ -54,23 +45,5 @@
 		// Remove our filter to avoid affecting other content
 		remove_filter('the_content', $append_link_filter);
 		?>
-		<!-- Comments disabled by default, can be enabled in Customizer -->
-		<?php if (is_single() && get_theme_mod('baseline_show_comments', false)) : ?>
-			<?php if (comments_open() || get_comments_number()) : ?>
-				<div class="divComments">
-					<?php comments_template(); ?>
-				</div>
-			<?php endif; ?>
-		<?php endif; ?>
 	</div>
-	<?php if (is_single()) : ?>
-		<?php $categories = get_filtered_categories(); ?>
-		<?php if ($categories) : ?>
-			<div class="divCategories">
-				Categories: <?php echo join(', ', array_map(function ($cat) {
-								return '<a href="' . esc_url(get_category_feed_link($cat->term_id)) . '">' . esc_html($cat->name) . '</a>';
-							}, $categories)); ?>.
-			</div>
-		<?php endif; ?>
-	<?php endif; ?>
 </div>
